ab9fdcda7c03865cda105fd5adb65c53da38605c
Ammad Ali <73386704+Mr-Nobody1@users.noreply.github.com>
2022-07-26 18:18:39 +0500

    Added All files

---
+++ app.js
@@ -0,0 +1,82 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require('ejs');
+
+const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+   var today = new Date();
+
+   res.render("list", {kinfOfDay: today, newListItem: items});
+   
+
+
+});
+
+app.post("/", function(req, res){
+   var item = req.body.newItem;
+   items.push(item);
+   
+   res.redirect("/");
+   
+})
+
+app.post("/about", function(req, res){
+   res.render("about");
+})
+
+app.listen(3000, function(){
+    console.log("Server is running");
+});
+
+
+
+
+
+
+// const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    
+//     var currentDay = weekday[today.getDay()];
+// if(today.getDate() === 6 || today.getDate() === 0 ){
+//    day = currentDay ;
+
+// }else{
+//    day = currentDay;
+// }
+//    res.render("list", {kinfOfDay: currentDay});
+
+
+
+
+
+
+
+
+
+
+
+// app.post("/", function(req, res){
+//    var item = req.body.newItem;
+//    items.push(item);
+
+//    res.redirect("/");
+
+// });
+
+
+
+
+
+// var options = {   weekday: 'long', day: 'numeric', month: 'long' };
+
+// var day = today.toLocaleDateString('en-US', options);
+
+// res.render("list", {kinfOfDay: day , newListItem: items});
